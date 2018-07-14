db30e10ec73867376063a5a80aa6282347ba3d36
AspenJames <aspenjames@tqca.org>
2018-07-14 14:08:33 -0700

    modules defined in one file

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+mod client {
+    fn connect() {
+    }
+}
+
+mod network { // declare a module named network
+    // declare a function connect within the module
+    fn connect() {
+    }
+
+    mod server {
+        fn connect() {
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
